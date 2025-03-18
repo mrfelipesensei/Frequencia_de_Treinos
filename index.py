3ea78f55ea6f23f369cdc197c242a1d96280c42f
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-18 17:56:07 -0300

    dicionário com dados inseridos

--- index.py
+++ index.py
@@ -1,3 +1,12 @@
+#Definição dos inputs
 data = input("Insira a data do treino: ")
 grupo_muscular = input("Insira o Grupo Muscular: ")
-nome = input("Insira o nome do Exercício: ")
+nome = input("Insira o Nome do Exercício: ")
+
+#Criação de um dicionário com os dados inseridos
+treino = {
+    "data" : data,
+    "exercicios" : [{"grupo_muscular": grupo_muscular, "nome": nome}]
+}
+
+'''print(treino)'''
